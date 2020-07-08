6f6ad5a7b65a45ceae668b2fdbd833f5d5674121
factory67 <67993461+factory67@users.noreply.github.com>
2020-07-08 19:04:25 +0700

    Add files via upload

---
+++ charp A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "   #" << endl;
+	cout << "  # #" << endl;
+	cout << " #   #" << endl;
+	cout << "#     #" << endl;
+	cout << "#######" << endl;
+	cout << "#     #" << endl;
+	cout << "Name:Natdanai Jansomboon" << endl;
+	return 0;
+}
